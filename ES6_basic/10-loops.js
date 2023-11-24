6d2e83c08eabb53dd4ce3a0093c20e04e0e89acb
jocalao001 <113933963+jocalao001@users.noreply.github.com>
2023-11-24 18:42:38 -0500

    add ES6_basic files

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const lis = [];
+    for (const idx of array) {
+      lis.push(appendString + idx);
+    }
+  
+    return lis;
+  }
