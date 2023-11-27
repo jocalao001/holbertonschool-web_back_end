8340c1d4d7e2f340d1847abfbdcceaf235ddb7dc
jocalao001 <113933963+jocalao001@users.noreply.github.com>
2023-11-27 12:12:38 -0500

    fix identation

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,5 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-    return Promise.race([chinaDownload, USDownload])
-        .then((response) => response)
-        .catch((err) => err);
+  return Promise.race([chinaDownload, USDownload])
+    .then((response) => response)
+    .catch((err) => err);
 }
--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,11 +1,11 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
-    try {
-        queue.push(mathFunction());
-    } catch (e) {
-        const { name, message } = e;
-        queue.push(`${name}: ${message}`);
-    }
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (e) {
+    const { name, message } = e;
+    queue.push(`${name}: ${message}`);
+  }
     queue.push('Guardrail was processed');
-    return queue;
+  return queue;
 }
