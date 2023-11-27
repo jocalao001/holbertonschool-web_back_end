f562ca34892e25c5c00a399f6b4932ffad96561e
jocalao001 <113933963+jocalao001@users.noreply.github.com>
2023-11-27 12:37:42 -0500

    Create 10-update_uniq_items.js

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,9 @@
+export default function updateUniqueItems(map) {
+  if (map && map instanceof Map) {
+    for (const [k, v] of map) {
+      if (v === 1) map.set(k, 100);
+    }
+    return map;
+  }
+  throw new Error('Cannot process');
+}
